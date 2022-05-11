9d6324f35432b76887c42be3ed22ef5d3a511ad7
Felipa Mendez <felipalouise@gmail.com>
2022-05-10 20:07:08 -0500

    commit tree 59b6c4b77d8cecb60df4cce96459100804a11b87

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,5 +1,22 @@
 require_relative './node'
 
 class LinkedList
+    attr_accessor :head
+
+    def initialize 
+        @head = nil
+    end
+
+    def append
+        if head.nil?
+            self.head = node
+        end
+
+        last_node = head
+        while last_node.next_node
+            last_node = last_node.next_node
+        end
+        last_node.next_node = node
+    end
 
 end
